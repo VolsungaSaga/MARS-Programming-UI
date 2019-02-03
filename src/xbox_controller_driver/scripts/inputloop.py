ee091ab4e78f229463cf17456660cf6040313a20
David Dimmett <dcd2sg@virginia.edu>
2019-02-02 19:08:17 -0500

    fixed typo

--- src/xbox_controller_driver/scripts/inputloop.py
+++ src/xbox_controller_driver/scripts/inputloop.py
@@ -93,7 +93,7 @@ if __name__=="__main__":
                 controllerState.rightTrigger=controller.rightTrigger()
                 #controller.refresh()
                 rospy.loginfo(controllerState)
-                pub.publish(ControllerState)
+                pub.publish(controllerState)
                 rate.sleep()
     except rospy.ROSInterruptException:
         print('InterruptException')
